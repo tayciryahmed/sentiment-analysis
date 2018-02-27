90dcea3e81d4982ddebd410b5520b6a87dafabac
ThomasGauth <thomas.gauthier00@gmail.com>
2018-02-27 14:08:39 +0100

    Add confusion matrix computation

--- experiments/RecursiveNN/test_network_loader.py
+++ experiments/RecursiveNN/test_network_loader.py
@@ -7,7 +7,7 @@ from sklearn.metrics import accuracy_score
 import numpy as np
 le = preprocessing.LabelEncoder()
 
-sys.path.append('experiments/RecursiveNN')
+from utils import plot_confusion_matrix
 
 from classifier import RNN_Model
 from utils import Vocab, clean_text_simple
@@ -47,9 +47,29 @@ def predict_proba(X,model):
         y = np.append(y, root_prediction.data.numpy(), axis=0)
     return y
 
+def accuracy_per_class(y, y_pred, classes):
+    n_classes = len(classes)
+    class_correct = list(0. for i in range(n_classes))
+    class_total = list(0. for i in range(n_classes))
+    # (real, predicted)
+    confusion_matrix = np.zeros((n_classes, n_classes), dtype=np.int64)
 
-X_train, y_train = load_data('data/train.csv')
-X_valid, y_valid = load_data('data/valid.csv', test=True)
+    for i in range(y.shape[0]):
+        label = y[i]
+        confusion_matrix[label, y_pred[i]] += 1
+        if y[i] == y_pred[i]:
+            class_correct[label] += 1
+        class_total[label] += 1
+
+    print("{:<10} {:^10}".format("Class", "Accuracy (%)"))
+    for i in range(n_classes):
+        print('{:<10} {:^10.2f}'.format(
+            classes[i], 100 * class_correct[i] / class_total[i]))
+    return confusion_matrix
+
+
+X_train, y_train = load_data('../../data/train.csv')
+X_valid, y_valid = load_data('../../data/valid.csv', test=True)
 
 train_tokens = pd.Series(X_train).apply(clean_text_simple)
 train_data = tr.loadTrees(train_tokens,y_train)
@@ -59,11 +79,23 @@ vocab = Vocab()
 vocab.construct(list(itertools.chain.from_iterable(train_sents)))
         
 test_net = RNN_Model(vocab,embed_size = 300)
-model_path = "experiments/Recursive NN/trainings/model_epoch17.pth"
-test_net.load_state_dict(torch.load(model_path))
 
-y_pred_train = predict(X_train,test_net)
-print("Train:", accuracy_score(y_train, y_pred_train))
+for model in range(29,30):
+    model_path = "poids_entrainement/recursiveNN_model_epoch{}.pth".format(model)
+    test_net.load_state_dict(torch.load(model_path))
 
-y_pred = predict(X_valid,test_net)
-print("Val:", accuracy_score(y_valid, y_pred))
+    y_pred_train = predict(X_train,test_net)
+    print("Train: iter", model,  accuracy_score(y_train, y_pred_train))
+
+    classes = ('negative', 'neutral', 'positive')
+    confusion_matrix_train = accuracy_per_class(y_train, y_pred_train, classes)
+    # Plot normalized confusion matrix
+    plot_confusion_matrix(confusion_matrix_train, classes, normalize=True,
+                      title='Normalized confusion matrix, train')
+
+    y_pred = predict(X_valid,test_net)
+    print("Val: iter", model, accuracy_score(y_valid, y_pred))
+    confusion_matrix_pred = accuracy_per_class(y_valid, y_pred, classes)
+    # Plot normalized confusion matrix
+    plot_confusion_matrix(confusion_matrix_pred, classes, normalize=True,
+                      title='Normalized confusion matrix, val')
--- experiments/RecursiveNN/utils.py
+++ experiments/RecursiveNN/utils.py
@@ -8,6 +8,9 @@ from nltk.stem import SnowballStemmer
 from sklearn import preprocessing
 le = preprocessing.LabelEncoder()
 
+import matplotlib.pyplot as plt
+import itertools
+
 class Vocab(object):
     def __init__(self):
         self.word_to_index = {}
@@ -93,3 +96,39 @@ def arrange_embeddings(embeddings, vocab, embed_size):
         if vector is not None:
             arranged_embeddings[index] = vector
     return arranged_embeddings
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    from http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+    :param cm: (numpy matrix) confusion matrix
+    :param classes: [str]
+    :param normalize: (bool)
+    :param title: (str)
+    :param cmap: (matplotlib color map)
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.figure(figsize=(8, 8))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
